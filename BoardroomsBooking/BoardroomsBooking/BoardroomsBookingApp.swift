58c8aa868b2345f5fdf00154ceb7e6e0a325c634
wteen alghamdy <wteenalghamdy1@gmail.com>
2025-12-24 14:56:01 +0300

    project created

---
+++ BoardroomsBooking/BoardroomsBooking/BoardroomsBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomsBookingApp.swift
+//  BoardroomsBooking
+//
+//  Created by Wteen Alghamdy on 04/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BoardroomsBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
