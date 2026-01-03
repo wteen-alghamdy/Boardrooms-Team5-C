b8cebbdb81b81b246a1d3331430282f33a4f16b3
wteen alghamdy <wteenalghamdy1@gmail.com>
2026-01-03 23:25:28 +0300

    bliiked the weekends (the office is closed on the weekends)

--- BoardroomsBooking/BoardroomsBooking/Main/MainViewModel.swift
+++ BoardroomsBooking/BoardroomsBooking/Main/MainViewModel.swift
@@ -7,6 +7,67 @@
 
 
 
+//
+//import Foundation
+//import Combine
+//
+//@MainActor
+//class MainViewModel: ObservableObject {
+//    @Published var boardrooms: [BoardroomRecord] = []
+//    @Published var isLoading = false
+//    
+//    // متغيرات التاريخ الديناميكية
+//    @Published var currentMonthName: String = ""
+//    
+//    @Published var calendarDays: [(dayName: String, dateNumber: String)] = []
+//    private let urlString = "https://api.airtable.com/v0/appElKqRPusTLsnNe/boardrooms"
+//    private let token = "Bearer pat7E88yW3dgzlY61.2b7d03863aca9f1262dcb772f7728bd157e695799b43c7392d5faf4f52fcb001"
+//
+//    init() {
+//        generateCurrentMonthCalendar()
+//    }
+//
+//    
+//    func generateCurrentMonthCalendar() {
+//        let calendar = Calendar.current
+//        let today = Date()
+//        
+//        let monthFormatter = DateFormatter()
+//        monthFormatter.dateFormat = "MMMM"
+//        self.currentMonthName = monthFormatter.string(from: today)
+//        
+//        let dayFormatter = DateFormatter()
+//        dayFormatter.dateFormat = "EEE"
+//        
+//        var tempDays: [(dayName: String, dateNumber: String)] = []
+//        for i in 0..<14 {
+//            if let date = calendar.date(byAdding: .day, value: i, to: today) {
+//                let dayName = dayFormatter.string(from: date)
+//                let dateNumber = String(calendar.component(.day, from: date))
+//                tempDays.append((dayName: dayName, dateNumber: dateNumber))
+//            }
+//        }
+//        self.calendarDays = tempDays
+//    }
+//
+//
+//    func fetchData() async {
+//        isLoading = true
+//        guard let url = URL(string: urlString) else { return }
+//        var request = URLRequest(url: url)
+//        request.setValue(token, forHTTPHeaderField: "Authorization")
+//
+//        do {
+//            let (data, _) = try await URLSession.shared.data(for: request)
+//            let decoded = try JSONDecoder().decode(BoardroomResponse.self, from: data)
+//            self.boardrooms = decoded.records
+//        } catch {
+//            print("❌ Error fetching rooms: \(error)")
+//        }
+//        isLoading = false
+//    }
+//}
+
 
 import Foundation
 import Combine
@@ -15,11 +76,9 @@ import Combine
 class MainViewModel: ObservableObject {
     @Published var boardrooms: [BoardroomRecord] = []
     @Published var isLoading = false
-    
-    // متغيرات التاريخ الديناميكية
     @Published var currentMonthName: String = ""
-    
     @Published var calendarDays: [(dayName: String, dateNumber: String)] = []
+    
     private let urlString = "https://api.airtable.com/v0/appElKqRPusTLsnNe/boardrooms"
     private let token = "Bearer pat7E88yW3dgzlY61.2b7d03863aca9f1262dcb772f7728bd157e695799b43c7392d5faf4f52fcb001"
 
@@ -27,7 +86,6 @@ class MainViewModel: ObservableObject {
         generateCurrentMonthCalendar()
     }
 
-    
     func generateCurrentMonthCalendar() {
         let calendar = Calendar.current
         let today = Date()
@@ -50,7 +108,6 @@ class MainViewModel: ObservableObject {
         self.calendarDays = tempDays
     }
 
-
     func fetchData() async {
         isLoading = true
         guard let url = URL(string: urlString) else { return }
@@ -67,10 +124,6 @@ class MainViewModel: ObservableObject {
         isLoading = false
     }
 }
-
-
-
-
 
 
 
